6aefe1ebdaa8346ba922039522a62e4e16f1c87e
Diego Montaña Franco <dmfantares@gmail.com>
2025-01-22 12:34:38 -0500

    Initial commit: Creating Softserve Test Task project

---
+++ src/main/java/com/example/softserve_test_task_java/SoftserveTestTaskJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.softserve_test_task_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftserveTestTaskJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftserveTestTaskJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/softserve_test_task_java/SoftserveTestTaskJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.softserve_test_task_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftserveTestTaskJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
