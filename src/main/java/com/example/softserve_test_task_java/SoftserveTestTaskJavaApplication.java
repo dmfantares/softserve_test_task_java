a4e68b1febe15fae51d3031b9f1bd364443ac115
Diego Montaña Franco <dmfantares@gmail.com>
2025-01-23 00:02:32 -0500

    Logic to remove duplicated characters from a given string was implemented. Code was refactored. Also, UTs were added.

--- src/main/java/com/example/softserve_test_task_java/SoftserveTestTaskJavaApplication.java
+++ src/main/java/com/example/softserve_test_task_java/SoftserveTestTaskJavaApplication.java
@@ -1,13 +1,13 @@
 package com.example.softserve_test_task_java;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.example.softserve_test_task_java.utils.DuplicatedCharactersValidator;
 
-@SpringBootApplication
 public class SoftserveTestTaskJavaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SoftserveTestTaskJavaApplication.class, args);
-	}
 
+		var givenString = "AABBCCD112233";
+		System.out.print(DuplicatedCharactersValidator.optimizedDeleteDuplicatesFrom(givenString));
+
+	}
 }
